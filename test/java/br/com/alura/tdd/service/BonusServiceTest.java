108a22af2747156d8f620208669880ef9cace912
Diogo Dantas <diogo.dantas@live.com>
2022-02-07 21:44:17 -0300

    Adicionado método inicializar na classe BonusServiceTest.

--- test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,10 +1,12 @@
 package br.com.alura.tdd.service;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Funcionario;
@@ -12,18 +14,21 @@ import br.com.alura.tdd.modelo.Funcionario;
 class BonusServiceTest {
 
 	private BonusService bonus;
+	
+	@BeforeEach
+	public void inicializar() {
+		this.bonus = new BonusService();
+	}
 
 	@Test
 	void ValidaValoresAbaixoDoLimiteParaOBonus() {
-		bonus = new BonusService();
-		BigDecimal salario = bonus.calcularBonus(new Funcionario("Otavio", LocalDate.now(), new BigDecimal("8000")));
 
+		BigDecimal salario = bonus.calcularBonus(new Funcionario("Otavio", LocalDate.now(), new BigDecimal("8000")));
 		assertEquals(new BigDecimal("800.00"), salario);
 	}
 
 	@Test
 	void ValidaValoresAcimaDoLimiteParaOBonus() {
-		BonusService bonus = new BonusService();
 		assertThrows(IllegalArgumentException.class,
 				() -> bonus.calcularBonus(new Funcionario("Otavio", LocalDate.now(), new BigDecimal("25000"))));
 
@@ -31,9 +36,7 @@ class BonusServiceTest {
 
 	@Test
 	void ValidaValoresIgualAoLimiteParaOBonus() {
-		BonusService bonus = new BonusService();
 		BigDecimal salario = bonus.calcularBonus(new Funcionario("Otavio", LocalDate.now(), new BigDecimal("10000")));
-
 		assertEquals(new BigDecimal("1000.00"), salario);
 	}
 
